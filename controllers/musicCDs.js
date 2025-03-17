dad941663ca89333dc5c388c10496444314c466a
jerryspann <jerryspann0@gmail.com>
2025-03-17 10:21:48 -0700

    added new route in musicCDs js folder

--- controllers/musicCDs.js
+++ controllers/musicCDs.js
@@ -3,14 +3,23 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// we will build out our router logic here
+// GET /users/:userId/musicCds
 router.get('/', async (req, res) => {
     try {
-      res.render('musicCDs/index.ejs');
+        res.render('musicCDs/index.ejs');
     } catch (error) {
-      console.log(error);
-      res.redirect('/');
+        console.log(error);
+        res.redirect('/');
     }
-  });
+});
+
+// GET /users/:userId/musicCDs/new
+router.get('/new', async (req, res) => {
+    res.render('musicCDs/new.ejs');
+});
+
+// POST /users/:userId/musicCDs
+
+
 
 module.exports = router;
