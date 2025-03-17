a889ebea5d40f6e6fae6e8656ed47d71f18b6476
jerryspann <jerryspann0@gmail.com>
2025-03-17 13:23:52 -0700

    added delete route

--- controllers/musicCDs.js
+++ controllers/musicCDs.js
@@ -6,7 +6,10 @@ const User = require('../models/user.js');
 // GET /users/:userId/musicCds
 router.get('/', async (req, res) => {
     try {
-        res.render('musicCDs/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('musicCDs/index.ejs', {
+            myCDs: currentUser.myCDs,
+        });
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -21,14 +24,41 @@ router.get('/new', async (req, res) => {
 // POST /users/:userId/musicCDs
 router.post('/', async (req, res) => {
     try {
-      const currentUser = await User.findById(req.session.user._id);
-      currentUser.myCDs.push(req.body);
-      await currentUser.save();
-      res.redirect(`/users/${currentUser._id}/musicCDs`);
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.myCDs.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/musicCDs`);
     } catch (error) {
-      console.log(error);
-      res.redirect('/');
+        console.log(error);
+        res.redirect('/');
     }
-  });
+});
+
+// GET /users/:userId/musicCDs/:myCDId
+router.get('/:myCDId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const myCD = currentUser.myCDs.id(req.params.myCDId);        
+        res.render('musicCDs/show.ejs', {
+            myCD: myCD,    
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// DELETE /users/:userId/musicCDs/:nyCDsId
+router.delete('/:myCDId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.myCDs.id(req.params.myCDId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/musicCDs`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
